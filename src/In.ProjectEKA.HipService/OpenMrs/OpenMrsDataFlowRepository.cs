16fe18db4559a9b71801163b7651b9ea5a5531f0
David Viktor Koltai <davidviktor.koltai@thoughtworks.com>
2020-09-15 17:44:48 +0700

    HIP #63 | David | add openmrs dataflow repository

---
+++ src/In.ProjectEKA.HipService/OpenMrs/OpenMrsDataFlowRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using In.ProjectEKA.HipLibrary.DataFlow;
+
+namespace In.ProjectEKA.HipService.OpenMrs
+{
+    public class OpenMrsDataFlowRepository : IOpenMrsDataFlowRepository
+    {
+        private readonly IOpenMrsClient openMrsClient;
+
+        public OpenMrsDataFlowRepository(IOpenMrsClient openMrsClient)
+        {
+            this.openMrsClient = openMrsClient;
+        }
+
+        public async Task<Bundle> GetBundleForCareContext(string patientId, string linkedCareContextVisitType)
+        {
+            var path = DiscoveryPathConstants.OnCareCotextBundlePath;
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            if (!string.IsNullOrEmpty(patientId))
+            {
+                query["patientId"] = patientId;
+                query["visitType"] = linkedCareContextVisitType;
+            }
+            if (query.ToString() != "")
+            {
+                path = $"{path}?{query}";
+            }
+
+            var response = await openMrsClient.GetAsync(path);
+            var content = await response.Content.ReadAsStringAsync();
+
+            var bundle = new FhirJsonParser().Parse<Bundle>(content);
+
+            return bundle;
+        }
+    }
+}
---
+++ test/In.ProjectEKA.HipServiceTest/OpenMrs/OpenMrsDataFlowRepositoryTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using In.ProjectEKA.HipService.OpenMrs;
+using Moq;
+using Xunit;
+
+namespace In.ProjectEKA.HipServiceTest.OpenMrs
+{
+    [Collection("OpenMrs Data flow Repository Tests")]
+    public class OpenMrsDataFlowRepositoryTest
+    {
+        OpenMrsDataFlowRepository dataFlowRepository;
+
+        private readonly Mock<IOpenMrsClient> openmrsClientMock;
+
+        public OpenMrsDataFlowRepositoryTest()
+        {
+            openmrsClientMock = new Mock<IOpenMrsClient>();
+            dataFlowRepository = new OpenMrsDataFlowRepository(openmrsClientMock.Object);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task ShouldReturnBundle()
+        {
+            //Given
+            var patientId = "123";
+            var careContextReference = "OPD";
+            SetupOpenMrsClient();
+
+            //When
+            var bundle = await dataFlowRepository.GetBundleForCareContext(patientId, careContextReference);
+
+            //Then
+            Assert.NotNull(bundle);
+        }
+
+        private void SetupOpenMrsClient()
+        {
+            var openMrsBundleJsonResponse = @"{
+                ""resourceType"": ""Bundle"",
+                ""entry"": [
+                    {
+                        ""resource"": {
+                            ""resourceType"": ""MedicationRequest"",
+                        ""dosageInstruction"": [
+                            {
+                                ""doseAndRate"": [
+                                    {
+                                    ""doseQuantity"": {
+                                        ""value"": 3.0
+                                        }
+                                }
+                                ]
+                            }
+                        ],
+                        ""dispenseRequest"": {
+                                ""quantity"": {
+                                    ""value"": 30.0
+                                }
+                            }
+                        }
+                    }
+                ]
+            }";
+            openmrsClientMock
+                .Setup(x => x.GetAsync(It.IsAny<string>()))
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(openMrsBundleJsonResponse)
+                })
+                .Verifiable();
+        }
+    }
+}
