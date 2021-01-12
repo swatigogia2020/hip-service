9b9cf92db136076fd900026daf0f758c004e4023
Meghna Tanwal <61121405+MeghnaThoughtworks@users.noreply.github.com>
2021-01-12 15:57:00 +0530

    #90\Meghna | Changes the discovery to use new CareContext OMOD api (#39)
    * #90\Meghna | Changes the discovery to use new CareContext OMOD api
    
    * #90\Meghna | Refactors to add the care context directly in the discovery
    
    * #90\Meghna | Logs errors
    
    Co-authored-by: meghna <meghna.tanwal@thoughworks.com>

--- src/In.ProjectEKA.HipService/OpenMrs/OpenMrsCareContextRepository.cs
+++ src/In.ProjectEKA.HipService/OpenMrs/OpenMrsCareContextRepository.cs
@@ -1,42 +1,30 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Web;
 using In.ProjectEKA.HipLibrary.Patient;
 using In.ProjectEKA.HipLibrary.Patient.Model;
-using In.ProjectEKA.HipService.Logger;
 
 namespace In.ProjectEKA.HipService.OpenMrs
 {
     public class OpenMrsCareContextRepository : ICareContextRepository
     {
         private readonly IOpenMrsClient openMrsClient;
+
         public OpenMrsCareContextRepository(IOpenMrsClient openMrsClient)
         {
             this.openMrsClient = openMrsClient;
         }
 
-        public async Task<IEnumerable<CareContextRepresentation>> GetCareContexts(string patientReferenceNumber)
+        public async Task<IEnumerable<CareContextRepresentation>> GetCareContexts(string patientUuid)
         {
-            var combinedCareContexts = new List<CareContextRepresentation>();
-            var programCareContexts = await LoadProgramEnrollments(patientReferenceNumber);
-            combinedCareContexts.AddRange(programCareContexts);
-
-            var visitCareContexts = await LoadVisits(patientReferenceNumber);
-            combinedCareContexts.AddRange(visitCareContexts);
-
-            return combinedCareContexts;
-        }
-        public virtual async Task<List<CareContextRepresentation>> LoadProgramEnrollments(string uuid)
-        {
-            var path = DiscoveryPathConstants.OnProgramEnrollmentPath;
+            var path = DiscoveryPathConstants.OnCareContextPath;
             var query = HttpUtility.ParseQueryString(string.Empty);
-            if (!string.IsNullOrEmpty(uuid))
+            if (!string.IsNullOrEmpty(patientUuid))
             {
-                query["patient"] = uuid;
-                query["v"] = "full";
+                query["patientUuid"] = patientUuid;
             }
+
             if (query.ToString() != "")
             {
                 path = $"{path}?{query}";
@@ -44,73 +32,17 @@ namespace In.ProjectEKA.HipService.OpenMrs
 
             var response = await openMrsClient.GetAsync(path);
             var content = await response.Content.ReadAsStringAsync();
-
             var jsonDoc = JsonDocument.Parse(content);
             var root = jsonDoc.RootElement;
-
             var careContexts = new List<CareContextRepresentation>();
-            var results = root.GetProperty("results");
-            for (int i = 0; i < results.GetArrayLength(); i++)
+            
+            for (var i = 0; i < root.GetArrayLength(); i++)
             {
-                var attributes = TryGetProperty(results[i], "attributes");
-                if (attributes.GetArrayLength() == 0) {
-                    LogAndThrowException($"Property 'attributes' is empty when getting program enrollments.");
-                }
-                var referenceNumber = TryGetProperty(attributes[0], "value");
-                var display = TryGetProperty(results[i], "display");
-                careContexts.Add(new CareContextRepresentation(referenceNumber.GetString(), display.GetString()));
+                var careContextName = root[i].GetProperty("careContextName").GetString();
+                var careContextReferenceNumber = root[i].GetProperty("careContextReference").ToString();
+                careContexts.Add(new CareContextRepresentation(careContextReferenceNumber, careContextName));
             }
-
             return careContexts;
-        }
-
-        private JsonElement TryGetProperty(JsonElement data, string propertyName) {
-            if (!data.TryGetProperty(propertyName, out var property)) {
-                LogAndThrowException($"Property '{propertyName}' is missing when getting program enrollments.");
-            }
-            return property;
-        }
-
-        private void LogAndThrowException(string message) {
-            Log.Error(message);
-            throw new OpenMrsFormatException();
-        }
-
-        public virtual async Task<List<CareContextRepresentation>> LoadVisits(string uuid)
-        {
-            var path = DiscoveryPathConstants.OnVisitPath;
-            var query = HttpUtility.ParseQueryString(string.Empty);
-            if (!string.IsNullOrEmpty(uuid))
-            {
-                query["patient"] = uuid;
-                query["v"] = "full";
-            }
-            if (query.ToString() != "")
-            {
-                path = $"{path}/?{query}";
-            }
-
-            var response = await openMrsClient.GetAsync(path);
-            var content = await response.Content.ReadAsStringAsync();
-
-            var jsonDoc = JsonDocument.Parse(content);
-            var root = jsonDoc.RootElement;
-
-            var careContexts = new List<CareContextRepresentation>();
-            var results = root.GetProperty("results");
-            for (int i = 0; i < results.GetArrayLength(); i++)
-            {
-                var visitType = results[i].GetProperty("visitType");
-                var referenceNumber = visitType.GetProperty("display").GetString();
-                careContexts.Add(new CareContextRepresentation(referenceNumber, null));
-            }
-
-            List<CareContextRepresentation> uniqueCareContexts = careContexts
-                .GroupBy(careContext => careContext.ReferenceNumber)
-                .Select(visitType => visitType.First())
-                .ToList();
-
-            return uniqueCareContexts;
         }
     }
 }
--- test/In.ProjectEKA.HipServiceTest/OpenMrs/OpenMrsCareContextRepositoryTest.cs
+++ test/In.ProjectEKA.HipServiceTest/OpenMrs/OpenMrsCareContextRepositoryTest.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
-using In.ProjectEKA.HipLibrary.Patient.Model;
 using In.ProjectEKA.HipService.OpenMrs;
 using Moq;
 using Xunit;
@@ -25,136 +22,43 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs
         }
 
         [Fact]
-        public async Task ShouldReturnListOfProgramEnrollments()
-        {
-            //Given
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnProgramEnrollmentPath, programEnrollmentSampleFull);
-
-            //When
-            var programenrollments = await careContextRepository.LoadProgramEnrollments(null);
-
-            //Then
-            var program = programenrollments[0];
-            program.ReferenceNumber.Should().Be("12345");
-            program.Display.Should().Be("HIV Program");
-        }
-
-        [Theory]
-        [InlineData("\"attributes\":[],", ProgramDisplay)]
-        [InlineData("", ProgramDisplay)]
-        [InlineData(AttributesOfProgramEnrollmentWithoutValue, ProgramDisplay)]
-        [InlineData(AttributesOfProgramEnrollment, "")]
-        public void ShouldReturnErrorIfSomeFieldsAreMissing(string attribute, string display)
-        {
-            //Given
-            var invalidSample = ProgramEnrollmentSample(attribute, display);
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnProgramEnrollmentPath, invalidSample);
-
-            //When
-            Func<Task> loadProgramEnrollments = async () => { await careContextRepository.LoadProgramEnrollments(null); };
-
-            //Then
-            loadProgramEnrollments.Should().Throw<OpenMrsFormatException>();
-        }
-
-        [Fact]
-        public async Task ShouldReturnEmptyListIfNoProgramEnrollmentsCareContexts()
-        {
-            //Given
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnProgramEnrollmentPath, EmptySample);
-
-            //When
-            var programenrollments = await careContextRepository.LoadProgramEnrollments(null);
-
-            //Then
-            programenrollments.Count().Should().Be(0);
-        }
-
-        [Fact]
-        public async Task ShouldReturnListOfVisits()
-        {
-            //Given
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnVisitPath, VisitSample);
-
-            //When
-            var visits = await careContextRepository.LoadVisits(null);
-
-            //Then
-            var visit = visits[0];
-            visit.ReferenceNumber.Should().Be("OPD");
-            visit.Display.Should().Be(null);
-        }
-
-        [Fact]
-        public async Task ShouldReturnListOfVisitsGroupedByType()
-        {
-            //Given
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnVisitPath, VisitSample);
-
-            //When
-            var visits = await careContextRepository.LoadVisits(null);
-
-            //Then
-            var numberOfVisitTypes = visits.Count();
-            numberOfVisitTypes.Should().Be(2);
-            var firstVisitType = visits[0];
-            firstVisitType.ReferenceNumber.Should().Be("OPD");
-            var secondVisitType = visits[1];
-            secondVisitType.ReferenceNumber.Should().Be("Emergency");
-        }
-
-        [Fact]
-        public async Task ShouldReturnEmptyListIfNoVisitCareContexts()
-        {
-            //Given
-            OpenMrsClientReturnsCareContexts(Endpoints.OpenMrs.OnVisitPath, EmptySample);
-
-            //When
-            var visits = await careContextRepository.LoadVisits(null);
-
-            //Then
-            visits.Count().Should().Be(0);
-        }
-
-        [Fact]
         public async Task ShouldReturnCombinedListOfCareContexts()
         {
+            var path = Endpoints.OpenMrs.OnCareContextPath;
+            OpenMrsClientReturnsCareContexts(path, ProgramEnrollmentSampleFull);
+
             //Given
-            Mock<OpenMrsCareContextRepository> careContextRepository = new Mock<OpenMrsCareContextRepository>(openmrsClientMock.Object);
+            Mock<OpenMrsCareContextRepository> careContextRepository =
+                new Mock<OpenMrsCareContextRepository>(openmrsClientMock.Object);
             careContextRepository.CallBase = true;
-            careContextRepository
-                .Setup(x => x.LoadProgramEnrollments(It.IsAny<string>()))
-                .ReturnsAsync(
-                    new List<CareContextRepresentation>
-                    {
-                        new CareContextRepresentation("12345", "HIV Program")
-                    })
-                .Verifiable();
-
-
-            careContextRepository
-                .Setup(x => x.LoadVisits(It.IsAny<string>()))
-                .ReturnsAsync(
-                    new List<CareContextRepresentation>
-                    {
-                        new CareContextRepresentation("OPD", null),
-                        new CareContextRepresentation("Emergency", null)
-                    })
-                .Verifiable();
 
             //When
             var combinedCareContexts =
                 (await careContextRepository.Object.GetCareContexts(null)).ToList();
 
             //Then
-            combinedCareContexts.Count().Should().Be(3);
-            combinedCareContexts[0].ReferenceNumber.Should().Be("12345");
-            combinedCareContexts[0].Display.Should().Be("HIV Program");
+            combinedCareContexts.Count().Should().Be(1);
+            combinedCareContexts[0].ReferenceNumber.Should().Be("4");
+            combinedCareContexts[0].Display.Should().Be("OPD");
+        }
 
-            combinedCareContexts[1].ReferenceNumber.Should().Be("OPD");
-            combinedCareContexts[1].Display.Should().Be(null);
-            combinedCareContexts[2].ReferenceNumber.Should().Be("Emergency");
-            combinedCareContexts[2].Display.Should().Be(null);
+        [Fact]
+        public async Task ShouldReturnEmptyListWhenNoCareContextsFound()
+        {
+            var path = Endpoints.OpenMrs.OnCareContextPath;
+            OpenMrsClientReturnsCareContexts(path, EmptySample);
+
+            //Given
+            Mock<OpenMrsCareContextRepository> careContextRepository =
+                new Mock<OpenMrsCareContextRepository>(openmrsClientMock.Object);
+            careContextRepository.CallBase = true;
+
+            //When
+            var combinedCareContexts =
+                (await careContextRepository.Object.GetCareContexts(null)).ToList();
+
+            //Then
+            combinedCareContexts.Count().Should().Be(0);
         }
 
         private void OpenMrsClientReturnsCareContexts(string path, string response)
@@ -169,179 +73,13 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs
                 .Verifiable();
         }
 
-        private readonly string programEnrollmentSampleFull = ProgramEnrollmentSample(AttributesOfProgramEnrollment, ProgramDisplay);
+        string ProgramEnrollmentSampleFull = @"[{
+            ""careContextType"": ""VISIT_TYPE"",
+            ""careContextName"": ""OPD"",
+            ""careContextReference"": 4}]";
 
-        private static readonly Func<string, string, string> ProgramEnrollmentSample = (attribute, display) => @"{
-            ""results"": [
-                {
-                    ""uuid"": ""c1720ca0-8ea3-4ef7-a4fa-a7849ab99d87"",
-                    ""patient"": {
-                        ""uuid"": ""b5e712bc-9472-41c0-a11f-500deac452d2"",
-                        ""display"": ""1234 - John Doe"",
-                        ""identifiers"": [
-                            {
-                                ""uuid"": ""af8996cb-e94b-463c-9ba0-17630dc12e0b"",
-                                ""display"": ""Patient Identifier = 1234"",
-                                ""links"": [
-                                    {
-                                        ""rel"": ""self"",
-                                        ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/patient/b5e712bc-9472-41c0-a11f-500deac452d2/identifier/af8996cb-e94b-463c-9ba0-17630dc12e0b""
-                                    }
-                                ]
-                            }
-                        ]
-                    }," + attribute + @"
-                    ""program"": {
-                        ""name"": ""HIV Program"",
-                        ""uuid"": ""5789a170-c020-4879-ae39-06b1de26cb5f"",
-                        ""retired"": false,
-                        ""description"": ""HIV Program"",
-                        ""concept"": {
-                            ""uuid"": ""ec41264d-e82e-4356-a7d2-7c1ff6c90abe"",
-                            ""display"": ""HIV"",
-                            ""links"": [
-                                {
-                                    ""rel"": ""self"",
-                                    ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/concept/ec41264d-e82e-4356-a7d2-7c1ff6c90abe""
-                                }
-                            ]
-                        }
-                    }," + display + @"
-                    ""dateEnrolled"": ""2020-07-13T14:00:00.000+0000"",
-                    ""dateCompleted"": null,
-                    ""location"": null,
-                    ""voided"": false,
-                    ""outcome"": null,
-                    ""links"": [
-                        {
-                            ""rel"": ""self"",
-                            ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/bahmniprogramenrollment/c1720ca0-8ea3-4ef7-a4fa-a7849ab99d87""
-                        }
-                    ],
-                    ""resourceVersion"": ""1.8""
-                }
-            ]
-        }";
 
-        private const string AttributesOfProgramEnrollmentWithoutValue = @"""attributes"": [
-                        {
-                            ""display"": ""ID_Number: 12345""
-                        }
-                    ],";
-
-        private const string AttributesOfProgramEnrollment = @"""attributes"": [
-                        {
-                            ""display"": ""ID_Number: 12345"",
-                            ""uuid"": ""11d5bc55-b94c-480c-ac0b-e5c9a7e40c20"",
-                            ""attributeType"": {
-                                ""uuid"": ""c41f844e-a707-11e6-91e9-0800270d80ce"",
-                                ""display"": ""ID_Number"",
-                                ""description"": ""ID Number"",
-                                ""retired"": false,
-                                ""links"": [
-                                    {
-                                        ""rel"": ""self"",
-                                        ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/programattributetype/c41f844e-a707-11e6-91e9-0800270d80ce""
-                                    }
-                                ]
-                            },
-                            ""value"": ""12345"",
-                            ""voided"": false,
-                            ""links"": [
-                                {
-                                    ""rel"": ""self"",
-                                    ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/bahmniprogramenrollment/57f76d2d-358e-4135-8160-247a94c49535/attribute/11d5bc55-b94c-480c-ac0b-e5c9a7e40c20""
-                                },
-                                {
-                                    ""rel"": ""full"",
-                                    ""uri"": ""http://192.168.33.10/openmrs/ws/rest/v1/bahmniprogramenrollment/57f76d2d-358e-4135-8160-247a94c49535/attribute/11d5bc55-b94c-480c-ac0b-e5c9a7e40c20?v=full""
-                                }
-                            ],
-                            ""resourceVersion"": ""1.9""
-                        }
-                    ],";
-
-        private const string ProgramDisplay = @"""display"": ""HIV Program"",";
-
-        private const string VisitSample = @"{
-            ""results"": [
-                {
-                    ""uuid"": ""fd377423-c804-4df2-b340-1ef844206769"",
-                    ""display"": ""OPD @ Odisha - 07/22/2020 11:28 AM"",
-                    ""patient"": {
-                        ""uuid"": ""2ceb3abb-1724-4f4f-b969-43b63f73545e"",
-                        ""display"": ""OD100012 - Anshul Test One"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/patient/2ceb3abb-1724-4f4f-b969-43b63f73545e""
-                            }
-                        ]
-                    },
-                    ""visitType"": {
-                        ""uuid"": ""96c49059-9af1-4f63-a8be-7554984fda02"",
-                        ""display"": ""OPD"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/visittype/96c49059-9af1-4f63-a8be-7554984fda02""
-                            }
-                        ]
-                    }
-                },
-                {
-                    ""uuid"": ""fd377423-c804-4df2-b340-1ef844206769"",
-                    ""display"": ""OPD @ Odisha - 07/22/2020 11:28 AM"",
-                    ""patient"": {
-                        ""uuid"": ""2ceb3abb-1724-4f4f-b969-43b63f73545e"",
-                        ""display"": ""OD100012 - Anshul Test One"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/patient/2ceb3abb-1724-4f4f-b969-43b63f73545e""
-                            }
-                        ]
-                    },
-                    ""visitType"": {
-                        ""uuid"": ""96c49059-9af1-4f63-a8be-7554984fda02"",
-                        ""display"": ""OPD"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/visittype/96c49059-9af1-4f63-a8be-7554984fda02""
-                            }
-                        ]
-                    }
-                },
-                {
-                    ""uuid"": ""fd377423-c804-4df2-b340-1ef844206769"",
-                    ""display"": ""OPD @ Odisha - 07/22/2020 11:28 AM"",
-                    ""patient"": {
-                        ""uuid"": ""2ceb3abb-1724-4f4f-b969-43b63f73545e"",
-                        ""display"": ""OD100012 - Anshul Test One"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/patient/2ceb3abb-1724-4f4f-b969-43b63f73545e""
-                            }
-                        ]
-                    },
-                    ""visitType"": {
-                        ""uuid"": ""96c49059-9af1-4f63-a8be-7554984fda02"",
-                        ""display"": ""Emergency"",
-                        ""links"": [
-                            {
-                                ""rel"": ""self"",
-                                ""uri"": ""http://bahmni-0.92.bahmni-covid19.in/openmrs/ws/rest/v1/visittype/96c49059-9af1-4f63-a8be-7554984fda02""
-                            }
-                        ]
-                    }
-                }
-            ]
-        }";
-
-        private const string EmptySample = @"{
-            ""results"": []
-        }";
+        private const string EmptySample = @"[]
+        ";
     }
 }
