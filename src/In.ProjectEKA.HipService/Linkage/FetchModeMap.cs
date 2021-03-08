97b50684138c7709b36fe219fb1f4452fde27351
swatigogia2020 <swati.gogia@thoughtworks.com>
2021-03-08 13:34:42 +0530

    Swati|Nazeer : checking in the intial file changes

---
+++ src/In.ProjectEKA.HipService/Linkage/FetchModeMap.cs
@@ -0,0 +1,11 @@
+namespace In.ProjectEKA.HipService.Linkage
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+
+    public static FetchModeMap{
+        public static Dictionary<string, string> requestIdToFetchMode = new Dictionary<string, string>(); 
+
+    }
+}
