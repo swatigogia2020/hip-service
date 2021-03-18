4c2a3ad8a78c02235735ccbaecc054b014df7366
meghna <meghna.tanwal@thoughworks.com>
2021-03-18 14:10:48 +0530

    129|Meghna|Sree Updates the tests for fetch mode controller

--- test/In.ProjectEKA.HipServiceTest/Linkage/FetchModeControllerTest.cs
+++ test/In.ProjectEKA.HipServiceTest/Linkage/FetchModeControllerTest.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using FluentAssertions;
 using Hl7.Fhir.Model;
-using In.ProjectEKA.HipService.Common;
+using In.ProjectEKA.HipLibrary.Patient.Model;
 using In.ProjectEKA.HipService.Gateway;
+using In.ProjectEKA.HipService.Link.Model;
 using In.ProjectEKA.HipService.Linkage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Constants = In.ProjectEKA.HipService.Common.Constants;
 
 namespace In.ProjectEKA.HipServiceTest.Linkage
 {
@@ -34,20 +37,56 @@ namespace In.ProjectEKA.HipServiceTest.Linkage
         {
             fetchModeController = new FetchModeController(gatewayClient.Object,
                 logger.Object,
-                gatewayConfiguration, 
+                gatewayConfiguration,
                 fetchModeService.Object);
         }
 
         [Fact]
         private void ShouldFetchPatientsAuthModes()
         {
-            var request = new FetchRequest("1234");
-            Requester requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
-            FetchQuery query = new FetchQuery(request.healthId, FETCH_MODE_PURPOSE, requester);
-            DateTime timeStamp = DateTime.Now.ToUniversalTime();
-            Guid requestId = Guid.NewGuid();
-            LinkageMap.RequestIdToFetchMode.Add(requestId, "12");
-            GatewayFetchModesRequestRepresentation gatewayFetchModesRequestRepresentation =
+            var request = new FetchRequest("hina_patel@ncg");
+            var requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
+            var query = new FetchQuery(request.healthId, FETCH_MODE_PURPOSE, requester);
+            var timeStamp = DateTime.Now.ToUniversalTime();
+            var requestId = Guid.NewGuid();
+            var gatewayFetchModesRequestRepresentation =
+                new GatewayFetchModesRequestRepresentation(requestId, timeStamp, query);
+            var correlationId = Uuid.Generate().ToString();
+            var modes = new List<Mode>
+            {
+                {Mode.MOBILE_OTP},
+                {Mode.AADHAAR_OTP}
+            };
+            var onFetchAuthModeRequest = new OnFetchAuthModeRequest(requestId, timeStamp,
+                new AuthModeFetch("KYC_AND_LINK", modes), null, new Resp(requestId.ToString()));
+            var authModes = string.Join(',', onFetchAuthModeRequest.Auth.Modes);
+            fetchModeService.Setup(a => a.FetchModeResponse(request, gatewayConfiguration))
+                .Returns(gatewayFetchModesRequestRepresentation);
+            gatewayClient.Setup(
+                    client =>
+                        client.SendDataToGateway(PATH_FETCH_AUTH_MODES,
+                            gatewayFetchModesRequestRepresentation, "ncg", correlationId))
+                .Returns(Task.FromResult(""))
+                .Callback<string, GatewayFetchModesRequestRepresentation, string, string>
+                ((path, gr, cmSuffix, corId)
+                    => fetchModeController.OnFetchAuthMode(onFetchAuthModeRequest));
+
+            if (fetchModeController.FetchPatientsAuthModes(correlationId, request).Result is OkObjectResult authMode)
+            {
+                authMode.StatusCode.Should().Be(StatusCodes.Status200OK);
+                authMode.Value.Should().BeEquivalentTo(authModes);
+            }
+        }
+
+        [Fact]
+        private void ShouldNotFetchPatientsAuthModes()
+        {
+            var request = new FetchRequest("hina_patel@ncg");
+            var requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
+            var query = new FetchQuery(request.healthId, FETCH_MODE_PURPOSE, requester);
+            var timeStamp = DateTime.Now.ToUniversalTime();
+            var requestId = Guid.NewGuid();
+            var gatewayFetchModesRequestRepresentation =
                 new GatewayFetchModesRequestRepresentation(requestId, timeStamp, query);
             var correlationId = Uuid.Generate().ToString();
 
@@ -55,14 +94,23 @@ namespace In.ProjectEKA.HipServiceTest.Linkage
                 .Returns(gatewayFetchModesRequestRepresentation);
             gatewayClient.Setup(
                     client =>
-                        client.SendDataToGateway(Constants.PATH_FETCH_AUTH_MODES,
+                        client.SendDataToGateway(PATH_FETCH_AUTH_MODES,
                             gatewayFetchModesRequestRepresentation, "ncg", correlationId))
                 .Returns(Task.FromResult(""));
 
-            if (fetchModeController.FetchPatientsAuthModes(correlationId, request).Result is OkObjectResult authMode)
+            try
             {
-                authMode.StatusCode.Should().Be(StatusCodes.Status200OK);
-                authMode.Value.Should().BeEquivalentTo("12");
+                fetchModeController.FetchPatientsAuthModes(correlationId, request);
+            }
+            catch (AggregateException ae)
+            {
+                if (ae.InnerException != null && ae.InnerException.GetType() == typeof(TimeoutException))
+                {
+                    Assert.True(true);
+                    Assert.Contains("Timeout for request_id: " + requestId, ae.Message);
+                }
+                else
+                    Assert.False(false);
             }
         }
     }
