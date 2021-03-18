afc7224c47e4eb880164d51ed44d1155563d5088
meghna <meghna.tanwal@thoughworks.com>
2021-03-18 13:54:02 +0530

    129|Meghna|Sree Updates the tests for auth init controller

--- test/In.ProjectEKA.HipServiceTest/Linkage/AuthInitControllerTest.cs
+++ test/In.ProjectEKA.HipServiceTest/Linkage/AuthInitControllerTest.cs
@@ -1,11 +1,14 @@
 using FluentAssertions;
 using Hl7.Fhir.Model;
+using In.ProjectEKA.HipLibrary.Patient.Model;
 using In.ProjectEKA.HipService.Common;
 using In.ProjectEKA.HipService.Gateway;
+using In.ProjectEKA.HipService.Link.Model;
 using In.ProjectEKA.HipService.Linkage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Meta = In.ProjectEKA.HipService.Link.Model.Meta;
 
 
 namespace In.ProjectEKA.HipServiceTest.Linkage
@@ -37,36 +40,78 @@ namespace In.ProjectEKA.HipServiceTest.Linkage
             authInitController = new AuthInitController(
                 gatewayClient.Object,
                 logger.Object,
-                gatewayConfiguration, 
+                gatewayConfiguration,
                 authInitService.Object);
         }
 
         [Fact]
         private void ShouldSendAuthInitRequest()
         {
-            var request = new AuthInitRequest("123", "12344");
-            
-            DateTime timeStamp = DateTime.Now.ToUniversalTime();
-            Requester requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
-            AuthInitQuery query = new AuthInitQuery(request.healthId, FETCH_MODE_PURPOSE, request.authMode, requester);
-            Guid requestId = Guid.NewGuid();
-            LinkageMap.RequestIdToTransactionIdMap.Add(requestId, "12");
-            GatewayAuthInitRequestRepresentation gatewayAuthInitRequestRepresentation =
+            var request = new AuthInitRequest("hina_patel@ncg", "MOBILE_OTP");
+
+            var timeStamp = DateTime.Now.ToUniversalTime();
+            var requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
+            var query = new AuthInitQuery(request.healthId, FETCH_MODE_PURPOSE, request.authMode, requester);
+            var requestId = Guid.NewGuid();
+            var gatewayAuthInitRequestRepresentation =
                 new GatewayAuthInitRequestRepresentation(requestId, timeStamp, query);
             var correlationId = Uuid.Generate().ToString();
-            
+            var transactionId = new Guid().ToString();
+            var auth = new Auth(transactionId, new Meta("string", new DateTime()), Mode.MOBILE_OTP);
+            var authOnInitRequest =
+                new AuthOnInitRequest(requestId, timeStamp, auth, null, new Resp(requestId.ToString()));
             authInitService.Setup(a => a.AuthInitResponse(request, gatewayConfiguration))
                 .Returns(gatewayAuthInitRequestRepresentation);
             gatewayClient.Setup(
-                client =>
-                    client.SendDataToGateway(PATH_AUTH_INIT,
-                        gatewayAuthInitRequestRepresentation, "ncg", correlationId))
+                    client =>
+                        client.SendDataToGateway(PATH_AUTH_INIT,
+                            gatewayAuthInitRequestRepresentation, "ncg", correlationId))
+                .Callback<string, GatewayAuthInitRequestRepresentation, string, string>
+                ((path, gr, cmSuffix, corId)
+                    => authInitController.OnAuthInit(authOnInitRequest))
                 .Returns(Task.FromResult(""));
 
-            if (authInitController.AuthInit(correlationId, request).Result is OkObjectResult transactionId)
+            if (authInitController.AuthInit(correlationId, request).Result is OkObjectResult tId)
             {
-                transactionId.StatusCode.Should().Be(StatusCodes.Status200OK);
-                transactionId.Value.Should().BeEquivalentTo("12");
+                tId.StatusCode.Should().Be(StatusCodes.Status200OK);
+                tId.Value.Should().BeEquivalentTo(transactionId);
+            }
+        }
+
+        [Fact]
+        private void ShouldNotSendAuthInitAndOnAuthInit()
+        {
+            var request = new AuthInitRequest("123", "12344");
+
+            var timeStamp = DateTime.Now.ToUniversalTime();
+            var requester = new Requester(gatewayConfiguration.ClientId, FETCH_MODE_REQUEST_TYPE);
+            var query = new AuthInitQuery(request.healthId, FETCH_MODE_PURPOSE, request.authMode, requester);
+            var requestId = Guid.NewGuid();
+            var gatewayAuthInitRequestRepresentation =
+                new GatewayAuthInitRequestRepresentation(requestId, timeStamp, query);
+            var correlationId = Uuid.Generate().ToString();
+
+            authInitService.Setup(a => a.AuthInitResponse(request, gatewayConfiguration))
+                .Returns(gatewayAuthInitRequestRepresentation);
+            gatewayClient.Setup(
+                    client =>
+                        client.SendDataToGateway(Constants.PATH_AUTH_INIT,
+                            gatewayAuthInitRequestRepresentation, "ncg", correlationId))
+                .Returns(Task.FromResult(""));
+
+            try
+            {
+                authInitController.AuthInit(correlationId, request);
+            }
+            catch (AggregateException ae)
+            {
+                if (ae.InnerException != null && ae.InnerException.GetType() == typeof(TimeoutException))
+                {
+                    Assert.True(true);
+                    Assert.Contains("Timeout for request_id: " + requestId, ae.Message);
+                }
+                else
+                    Assert.False(false);
             }
         }
     }
