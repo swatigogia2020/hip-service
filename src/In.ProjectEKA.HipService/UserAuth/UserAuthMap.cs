6b9b759496ef6fbd1fb2387d151d4f958e71cfa0
meghna <meghna.tanwal@thoughworks.com>
2021-05-18 13:41:46 +0530

    158 | Adds error handling for user auth apis sent from the gateway

--- src/In.ProjectEKA.HipService/UserAuth/UserAuthMap.cs
+++ src/In.ProjectEKA.HipService/UserAuth/UserAuthMap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using In.ProjectEKA.HipLibrary.Patient.Model;
 using In.ProjectEKA.HipService.Link.Model;
 using In.ProjectEKA.HipService.UserAuth.Model;
 
@@ -11,5 +12,6 @@ namespace In.ProjectEKA.HipService.UserAuth
         public static Dictionary<Guid, string> RequestIdToAccessToken = new Dictionary<Guid, string>();
         public static Dictionary<string, string> HealthIdToTransactionId = new Dictionary<string, string>();
         public static Dictionary<Guid, AuthConfirmPatient> RequestIdToPatientDetails = new Dictionary<Guid, AuthConfirmPatient>();
+        public static Dictionary<Guid, Error> RequestIdToErrorMessage = new Dictionary<Guid, Error>();
     }
 }
