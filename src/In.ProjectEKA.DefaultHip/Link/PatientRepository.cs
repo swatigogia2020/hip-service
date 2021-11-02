779a296e9daf0a6d68a3ec0ae83aef3dac8edc1e
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2021-11-02 13:03:12 +0530

    [Rohit|Tazeen] Resolves interface dependency in patient discovery changes

--- src/In.ProjectEKA.DefaultHip/Link/PatientRepository.cs
+++ src/In.ProjectEKA.DefaultHip/Link/PatientRepository.cs
@@ -5,13 +5,12 @@ namespace In.ProjectEKA.DefaultHip.Link
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using HipLibrary.Patient;
     using HipLibrary.Patient.Model;
     using Optional;
     using System.Threading.Tasks;
     using Patient;
 
-    public class PatientRepository : IPatientRepository
+    public class PatientRepository
     {
         private readonly string filePath;
 
@@ -38,22 +37,6 @@ namespace In.ProjectEKA.DefaultHip.Link
         {
             var patientsInfo = FileReader.ReadJson(filePath);
             return patientsInfo;
-        }
-
-        public Task<IQueryable<Patient>> PatientsWithVerifiedId(string healthId, string name, AdministrativeGender? gender, string yearOfBirth,
-            string phoneNumber)
-        {
-            return null;
-        }
-
-        public Task<IQueryable<Patient>> PatientsWithVerifiedId(string healthId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IQueryable<Patient>> PatientsWithDemographics(string name, AdministrativeGender? gender, string yearOfBirth, string phoneNumber)
-        {
-            throw new NotImplementedException();
         }
     }
 }
