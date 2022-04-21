d1c506b8785b51d36d42f66a0561cf067a9c7557
SanoferSameera <sanofersameera25@gmail.com>
2022-04-21 17:56:40 +0530

    BAH-1610 | Refactor. get patient visit data query to have visit_startDate as param
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- src/In.ProjectEKA.HipService/DataFlow/OpenMrsPatientData.cs
+++ src/In.ProjectEKA.HipService/DataFlow/OpenMrsPatientData.cs
@@ -65,8 +65,10 @@ namespace In.ProjectEKA.HipService.DataFlow
                 !string.IsNullOrEmpty(fromDate)
             )
             {
+                var careContexName = grantedContext.Split(" / ");
                 query["patientId"] = consentId;
-                query["visitType"] = grantedContext;
+                query["visitType"] = careContexName[0];
+                query["visitStartDate"] = careContexName[1];
                 query["fromDate"] = fromDate;
                 query["toDate"] = DateTime.Parse(toDate).AddDays(1).ToString("yyyy-MM-dd");
             }
