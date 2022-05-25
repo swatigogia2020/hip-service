8436f986cbf2f85176cbf19bfa13cda0b6a30f20
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2022-05-25 15:08:10 +0530

    Gateway response change | Add monthOfBirth and dayOfBirth to AuthConfirm Patient (#96)
    * Refactor. to map month and date of birth from authConfirm response
    
    * Refactor. to fix test failures

--- src/In.ProjectEKA.HipService/UserAuth/Model/AuthConfirmPatient.cs
+++ src/In.ProjectEKA.HipService/UserAuth/Model/AuthConfirmPatient.cs
@@ -7,17 +7,21 @@ namespace In.ProjectEKA.HipService.UserAuth.Model
         public string id { get; }
         public string name { get; }
         public string gender { get; }
-        public string yearOfBirth { get; }
+        public int yearOfBirth { get; }
+        public int monthOfBirth { get; }
+        public int dayOfBirth {get; }
         public AuthConfirmAddress address { get; }
         public List<Identifier> identifiers { get; }
 
-        public AuthConfirmPatient(string id, string name, string gender, string yearOfBirth, AuthConfirmAddress address,
+        public AuthConfirmPatient(string id, string name, string gender, int yearOfBirth, int monthOfBirth, int dayOfBirth, AuthConfirmAddress address,
             List<Identifier> identifiers)
         {
             this.id = id;
             this.name = name;
             this.gender = gender;
             this.yearOfBirth = yearOfBirth;
+            this.monthOfBirth = monthOfBirth;
+            this.dayOfBirth = dayOfBirth;
             this.address = address;
             this.identifiers = identifiers;
         }
