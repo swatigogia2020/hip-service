74d25a387a9eb78efdeb8313d985167d0ac7dd7e
SanoferSameera <sanofersameera25@gmail.com>
2022-06-30 19:05:38 +0530

    BAH-1931 | Fixed. To handle month and dayOfBirth gettig as null

--- src/In.ProjectEKA.HipService/UserAuth/Model/AuthConfirmPatient.cs
+++ src/In.ProjectEKA.HipService/UserAuth/Model/AuthConfirmPatient.cs
@@ -8,20 +8,20 @@ namespace In.ProjectEKA.HipService.UserAuth.Model
         public string name { get; }
         public string gender { get; }
         public int yearOfBirth { get; }
-        public int monthOfBirth { get; }
-        public int dayOfBirth {get; }
+        public int? monthOfBirth { get; }
+        public int? dayOfBirth {get; }
         public AuthConfirmAddress address { get; }
         public List<Identifier> identifiers { get; }
 
-        public AuthConfirmPatient(string id, string name, string gender, int yearOfBirth, int monthOfBirth, int dayOfBirth, AuthConfirmAddress address,
+        public AuthConfirmPatient(string id, string name, string gender, int yearOfBirth, int? monthOfBirth, int? dayOfBirth, AuthConfirmAddress address,
             List<Identifier> identifiers)
         {
             this.id = id;
             this.name = name;
             this.gender = gender;
             this.yearOfBirth = yearOfBirth;
-            this.monthOfBirth = monthOfBirth;
-            this.dayOfBirth = dayOfBirth;
+            this.monthOfBirth = monthOfBirth ?? 1;
+            this.dayOfBirth = dayOfBirth ?? 1;
             this.address = address;
             this.identifiers = identifiers;
         }
